8ca9e05f5db96c49cf67ed90cbdf98d8085f2da7
ruchajoshi375 <160557242+ruchajoshi375@users.noreply.github.com>
2024-03-12 17:33:25 -0400

    Add files via upload
    My Portfolio using ReactJS.

---
+++ App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+
+//JSX= JavaScript XML
+function App() {
+  return (
+    <div className="App">
+      <h1>My Portfolio</h1>
+      <strong>Name :Rucha Joshi</strong>
+      <p>Hello everyone. I am Rucha. I am currently a first year student studying at the University of Toronto.</p>
+      <p>Hobbies: Painting, Making coffee and Photography and my favourite subjects are Computer Science and Physics.</p>
+      <p>Skills: Front-end Web Dveloper. I can speak multiple languages including English, German, French, Marathi and Hindi.</p>
+    </div>
+  );
+}
+
+export default App;
